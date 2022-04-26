f4094313f62bf4fd2682a596d72a8832cbe53f7b
Cloud User <sasdemo@masnlp.hgfa0yw3papulameszjrtfw5xg.cx.internal.cloudapp.net>
2022-04-26 03:07:29 +0000

    edit rewriter.py

--- rewriter.py
+++ rewriter.py
@@ -5,16 +5,16 @@ from transformers import BertTokenizer
 from transformers import BertForMaskedLM
 from torch.nn import functional as F
 import torchtext.vocab as vocab
+import sys
 
-text = "The world we live in is disintegrating into a place of malice and hatred, where we need hope and find it harder."
+
+text = 'A novel is a relatively long work of narrative fiction, typically written in prose and published as a book. The present English word for a long work of prose fiction derives from the Italian: novella for "new", "news", or "short story of something new", itself from the Latin: novella, a singular noun use of the neuter plural of novellus, diminutive of novus, meaning "new". Some novelists, including Nathaniel Hawthorne, Herman Melville, Ann Radcliffe, John Cowper Powys, preferred the term "romance" to describe their novels.'
 # all the punctuations will not be replaced
 punctuations = ["[CLS]","[UNK]","[MASK]","[SEP]","[PAD]","'",'"',";",":",",",".","?","/",">","<","{","}"]
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 mapping = tokenizer(text, return_tensors="pt")
 org_tokens = mapping["input_ids"][0]
-#org_tokens = org_tokens[torch.arange(org_tokens.size) != org_tokens["SEP"]]
-print(org_tokens)
 cache_dir = 'GloVe6B5429'
 glove = vocab.GloVe(name='6B', dim=300, cache=cache_dir)
 word_piece_embeddings = torch.load('word-piece-embedding.txt').t()
@@ -31,7 +31,7 @@ for i in range(len(org_tokens)):
 # iteratively replace the mask words
 i = 1
 tokens = copy.deepcopy(org_tokens)
-while len(mask_pos) > 0 and i <= 6:
+while len(mask_pos) > 0 and i <= len(tokens):
     i = i+1
     #pick a random word in k position and mask it
     pos = random.sample(mask_pos,1)
@@ -44,8 +44,8 @@ while len(mask_pos) > 0 and i <= 6:
     c = torch.sum(c, dim=0)
     Ru = word_piece_embeddings + c
     s = torch.cosine_similarity(Ru,Rx,dim=1)
-    σ = 0.974
-    k = 0.5
+    σ = 0.975
+    k = 0.1
     Penforce = -k*torch.max(torch.zeros(s.size(0)),(σ-s))
     #calculate the P(lm), it is a token_size * vocal_size matrix
     tokens[pos[0]] = tokenizer.mask_token_id
