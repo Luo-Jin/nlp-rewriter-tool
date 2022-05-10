db4d92a3fdc757984857745e22c0c89d2da9a76d
Luo-Jin <ostrich_jin@hotmail.com>
2022-05-10 13:14:40 +0800

    rename pycurses.py to rewriter.py

--- rewriter.py
+++ rewriter.py
@@ -1,199 +1,308 @@
 ###############################################
-#   File    : generator.py
+#   File    : rewriter.py
 #   Author  : Jin luo
-#   Date    : 2022-04-23
-#   Input   : generator.py -t <text> -s <similarity> -e <enforcement>
-#             s=0.972 and e=0.01 if not specified
-#   Output  : three revised copies of the original sentences
+#   Date    : 2022-04-29
+#   Input   : None
+#   Output  : None
 ###############################################
 '''
-Description:
-this script makes three copies of original sentence.
-Loop in each replaceable positions
-  1. mask a random position in those copies.
-  2. calculates:
-     a.the word prob Plm by using BERT MASK Language Model, get tensor[3,30k]
-     b.the enforcement prob Penforce by using pre-trained "word-piece-embeddings", get tensor[3,30k]
-     c.the proposed prob for word Pproposal = element-wise add Plm and Penforce, get tensor[3,30k]
-  3. sample words with top 3 probs in Pproposal, (we could just use the top1)
-     and fill the rank 1 word id in masked position in the first copy
-         fill the rank 2 word id  in masked position in the second copy
-         fill the rank 3 word id  in masked position in the third copy
-  4. decode the three copies
-End loop
+a GUI for the rewriter tool
 '''
-from transformers import BertTokenizer
-from transformers import BertForMaskedLM
-from os import system, name
-import sys
-import getopt
-import random
-import torch
-import copy
-import spacy
-import curses
+
+import curses, traceback
 from curses import wrapper
+from configparser import ConfigParser
+import spacy
+import torch
+import numpy as np
+import generator as gen
+import copy
+import os
 
+# config object
+config = ConfigParser()
+# text frame objects
+screen = None
+txt_box = None
+revised_box = None
+nlp = spacy.load("en_core_web_sm")
+# initialize sentences list
+replaced_sents = []
+# model parameters
+σ = 0
+k = 0
+batch = 1
 
-# cache_dir = 'GloVe6B5429'
-# glove = vocab.GloVe(name='6B', dim=300, cache=cache_dir)
-# download bert embeddings
-tokenizer = BertTokenizer.from_pretrained('Bert/vocabulary')
-model = BertForMaskedLM.from_pretrained("Bert/model/maskedLM")
-word_piece_embeddings = torch.load('sig_sml1_e1000_b5000_l0.5_weight.pt').t()
-# stanza.download('en')
-curses.initscr()
-progress_bar = curses.newwin(1, 100, 23, 35)
-progress_bar.box()
-progress_counter = 0
+class Textframe(object):
+    def __init__(self,stdscr,line,col,y,x,id):
+        self.frame_line = line     # 20 by default
+        self.frame_col = col       # 100 by default
+        self.frame_y = y           # frame coordinates y, 2 by default
+        self.frame_x = x           # frame coordinates x, 5 by default
+        self.text_line = 90        # line size
+        self.row_num = 0           # max row number
+        self.select_idx = 0        # selected sentence position
+        self.current_sent_idx = 0  # current sentence index
+        self.current_para_idx = 0  # current sentence index
+        self.current_sentences = None # current sentences
+        self.firstrow_idx = 0      # number of first row to print
+        self.offset_x = 4          # text margin size to the left and left side of frame
+        self.offset_y = 2          # text margin size to the top and bottom of frame
+        self.indent = 4            # first sentences indent
+        self.window_size = self.frame_line - self.offset_y -1      # shift window size
+        self.para_num = 0          # number of paragraphs
+        self.sent_num = 0          # number of sentences
+        self.screen = stdscr       # father window
+        self.quit = 0             # indicator to quit loop
+        self.__win = curses.newwin(self.frame_line, self.frame_col, y, x)   # windows object
+        self.__win.keypad(True)
+        self.__paragraphs = []     # text paragraphs contain sentences
+        self.__sentences = []      # text sentences
+        self.__id = id             # instance id
+        self.enterKey = None       # process enter key strike,
+        self.alt_s = None          # process alt+s,
+        self.u =None               # process 'u' strike
 
-def main():
-    arg_sim = None
-    arg_enf = None
-    arg_txt = None
-    arg_help = "{0} -t <text> -s <similarity> -e <enforcement>".format(sys.argv[0])
+    def getWin(self):
+        return self.__win
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "t:s:e:", ["help", "text=","similar=", "enforce="])
-    except:
-        print(arg_help)
-        sys.exit(2)
+    def getParagragh(self):
+        return self.__paragraphs
 
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print(arg_help)  # print the help message
-            sys.exit(2)
-        elif opt in ("-t", "--text"):
-            arg_txt = arg
-        elif opt in ("-s", "--similar"):
-            arg_sim = arg
-        elif opt in ("-e", "--enforce"):
-            arg_enf = arg
-    σ = 0.975 if arg_sim is None else float(arg_sim)
-    k = 0.1 if arg_enf is None else float(arg_enf)
-    # read text from specific txt file
-    texts = readtxt(arg_txt)
-    print(texts)
-    screen_clear()
-    print("\033[1;33mThe original sentence is :\033[0m")
-    print("\033[7m\n{}\n\033[0m".format(texts))
-    tokens = rewriter(texts, σ, k)
-    print("\033[1;33mThe sentence is revised with smooth parameter k={} "
-          "and similarity rate σ={} :\033[0m".format(k,σ))
-    for i in torch.arange(len(tokens["input_ids"])):
-        print("{}{}{}"
-              .format(i + 1
-                      , "."
-                      , tokenizer.decode(tokens["input_ids"][i], skip_special_tokens=True)))
+    def setText(self,texts):
+        # reset parameters
+        self.__paragraphs.clear()
+        self.row_num = 0           # max row number
+        self.select_idx = 0        # selected sentence position
+        self.current_sent_idx = 0  # current sentence index
+        self.current_para_idx = 0  # current sentence index
+        self.firstrow_idx = 0      # number of first row to print
+        self.para_num = 0          # number of paragraphs
+        self.sent_num = 0          # number of sentences
 
+        n = 0
+        for i in np.arange(len(texts)):
+            p = texts[i]
+            p = p[0:len(p) - 1]
+            doc = nlp(p)
+            sents = []
+            j = 0
+            for sent in doc.sents:
+                sents.append([sent.text, 0, 0, 0, i, j,n,0]) # ["txt",color,y,x,para_idx,sent_idx,sent_num,changed]
+                j = j + 1
+                n = n + 1
+            self.__paragraphs.append(sents)
+        self.sent_num = n
 
-def readtxt(txt):
-    f = open(txt, mode='r')
-    texts = f.readline()
-    #nlp = spacy.load("en_core_web_sm")
-    #doc = nlp(texts[0])
-    #sents = [[sent.text,0,0,0] for sent in doc.sents]
+    def selectSentence(self):
+        while True:
+            c = self.__win.getch()
+            if c == curses.KEY_UP:
+                self.firstrow_idx = np.max([(self.firstrow_idx - 1), 0])
+            elif c == curses.KEY_DOWN:
+                self.firstrow_idx = np.min([(self.firstrow_idx + 1),np.max([self.row_num - self.window_size + self.offset_y, 0])])
+            elif c == curses.KEY_RIGHT:
+                self.calcPosition(1)
+            elif c == curses.KEY_LEFT:
+                self.calcPosition(-1)
+            elif c == 159:           # alt+s, save changes to txt_box
+                self.alt_s()
+                if self.__id == 1:
+                    break
+            elif c == 117:           # 'u', undo changes.
+                self.u()
+            elif c == 10:            # Enter key.
+                self.enterKey(self.__id)
+            elif c == 113:           # 'q' , quit loop
+                break
+            elif c == curses.KEY_HOME:
+                x = 0
+            self.calcPosition(0)
+            self.printText()
+
+    def calcPosition(self,step):
+        # update selected sentences position and color
+        self.select_idx = self.select_idx + step
+        if self.select_idx > self.sent_num - 1:
+            self.select_idx = 0
+        if self.select_idx < 0:
+            self.select_idx = self.sent_num - 1
+        x = self.indent
+        y = 0
+        # calculate the sentences to display
+        sents = []
+        for para in self.__paragraphs:
+            for i in np.arange(len(para)):
+                total_size = x + len(para[i][0])
+                l = [self.text_line] * int(total_size / self.text_line)
+                l.append(total_size % self.text_line)
+                str_len = len(para[i][0])
+                st = 0
+                para[i][1] =(1 if para[i][6] == self.select_idx else (0 if para[i][7] == 0 else 2))
+                self.current_sent_idx = (i if para[i][6] == self.select_idx else self.current_sent_idx)
+                self.current_para_idx = (self.__paragraphs.index(para) if para[i][6] == self.select_idx else self.current_para_idx)
+
+                for j in np.arange(len(l)):
+                    et = min((l[j] - x), str_len) + st
+                    if j == 0:
+                        para[i][2] = y
+                        para[i][3] = x
+                    sents.append([y, x, para[i][0][st:et], para[i][1]])
+                    x = et - st + x if et - st + x < self.text_line else 0
+                    if x == 0:
+                        y = y + 1
+                    str_len = str_len - et + st
+                    st = et
+            y = y + 1
+            x = self.indent
+        self.current_sentences = sents
+
+    def printText(self):
+        # print the sentences
+        self.__win.clear()
+        for i in np.arange(len(self.current_sentences)):
+            if self.firstrow_idx <= self.current_sentences[i][0] and self.current_sentences[i][0] <= self.firstrow_idx + self.window_size - self.offset_y:
+                self.__win.addstr(self.current_sentences[i][0] - self.firstrow_idx + self.offset_y
+                                  , self.current_sentences[i][1] + self.offset_x
+                                  , str(self.current_sentences[i][2])
+                                  ,curses.color_pair(self.current_sentences[i][3]))
+            self.row_num = self.current_sentences[i][0]
+        self.updateStatus()
+        self.__win.refresh()
+
+    def updateStatus(self):
+        y   = txt_box.frame_y -1
+        y1  = revised_box.frame_y - 1
+        x   = txt_box.frame_x
+        x1  = revised_box.frame_x
+
+        if self.__id == 0:
+            color1 = 3
+            color2 = 0
+            border_color1 = curses.A_STANDOUT
+            border_color2 = curses.A_NORMAL
+
+        else:
+            color1 = 0
+            color2 = 3
+            border_color1 = curses.A_NORMAL
+            border_color2 = curses.A_STANDOUT
+        screen.clear()
+        screen.addstr(y , x,
+                           "{}-{} of total {} lines. sentence {} in paragraph {} is selected"
+                           .format(txt_box.firstrow_idx, np.min([txt_box.firstrow_idx + txt_box.window_size, txt_box.row_num])
+                                   , txt_box.row_num, txt_box.current_sent_idx, txt_box.current_para_idx)
+                           , curses.color_pair(color1))
+
+        screen.addstr(y1,x1
+                           , "Three new sentences will be generated: σ={} and k={}".format(σ, k)
+                           , curses.color_pair(color2))
+
+        screen.refresh()
+        txt_box.getWin().attron(border_color1)
+        txt_box.getWin().box()
+        txt_box.getWin().attroff(border_color1)
+        txt_box.getWin().refresh()
+        revised_box.getWin().attron(border_color2)
+        revised_box.getWin().box()
+        revised_box.getWin().attroff(border_color2)
+        revised_box.getWin().refresh()
+
+def readTXT(file):
+    f = open(file, mode='r')
+    texts = f.readlines()
     f.close()
     return texts
 
-# define our clear function
-def screen_clear():
-    if name == 'nt':
-        _ = system('cls')
-        # for mac and linux(here, os.name is 'posix')
-    else:
-        _ = system('clear')
-        # print out some text
+def conformChange():
+    sent = revised_box.getParagragh()[revised_box.current_para_idx][revised_box.current_sent_idx]
+    txt = str(sent[0])
+    replaced_sent = copy.deepcopy(txt_box.getParagragh()[txt_box.current_para_idx][txt_box.current_sent_idx])
+    replaced_sents.append(replaced_sent)
+    txt_box.getParagragh()[txt_box.current_para_idx][txt_box.current_sent_idx][0] = txt
+    txt_box.getParagragh()[txt_box.current_para_idx][txt_box.current_sent_idx][7] = 1
+    #revised_box.quit = 1
 
-def update_progress(progress):
-    rangex = (50 / float(100)) * progress
-    pos = int(rangex)
-    display = '#'
-    if pos != 0:
-        progress_bar.addstr(0, pos, "{}".format(display))
-        progress_bar.refresh()
+def refineSentence(id):
+    paragraph = txt_box.getParagragh()
+    tokens = gen.rewriter(paragraph[txt_box.current_para_idx][txt_box.current_sent_idx][0], σ, k)
+    sents = []
+    for i in torch.arange(len(tokens["input_ids"])):
+        sent = str(gen.tokenizer.decode(tokens["input_ids"][i],skip_special_tokens=True))
+        sent = sent + "\n"
+        sents.append(sent)
 
-def penforce(batch,pos,org_tokens,tokens,k,σ):
-    # calculate the Rx for original sentence 3 x 30k x 300
-    Rx = word_piece_embeddings[org_tokens["input_ids"][0]]
-    Rx = torch.sum(Rx, dim=0)
-    Rx = Rx.expand(batch, word_piece_embeddings.size(0)
-                   , word_piece_embeddings.size(1))
-    # calculate the Ru for changed sentence in 3 x 30k x 300
-    Ru = word_piece_embeddings[tokens["input_ids"]]
-    # remove masked word from Ru
-    Ru = torch.cat([t[torch.arange(t.size(0)) != pos[0]] for t in Ru[:]])
-    Ru = Ru.view(batch, int(Ru.size(0) / batch), word_piece_embeddings.size(1))
-    # sum rest words prob
-    Ru = torch.sum(Ru, dim=1)
-    Ru = torch.cat([t.expand(word_piece_embeddings.size(0)
-                             , word_piece_embeddings.size(1)) for t in Ru[:]])
-    # add all other possible words prob
-    Ru = Ru.view(batch, word_piece_embeddings.size(0)
-                 , word_piece_embeddings.size(1))
-    Ru = Ru + word_piece_embeddings
-    # compute similarities between original sentence with all possible sentences (30k)
-    s = torch.cosine_similarity(Ru, Rx, dim=2)
-    Penforce = torch.exp(-k * torch.max(torch.zeros(batch, word_piece_embeddings.size(0)), (float(σ) - s)))
-    return Penforce
+    revised_box.setText(sents)
+    revised_box.calcPosition(0)
+    revised_box.printText()
+    curses.beep()
+    if id == 0:
+        revised_box.selectSentence()
 
-def plm(pos,tokens):
-    # replace choosen word with [MASK]
-    tokens["input_ids"][:, pos[0]] = tokenizer.mask_token_id
-    logits = model(**tokens)
-    logits = logits.logits
-    softmax = torch.softmax(logits, dim=-1)
-    Plm = softmax[:, pos[0]]
-    return Plm
+def saveChange():
+    paras = txt_box.getParagragh()
+    txt = []
+    for para in paras:
+        sents = ""
+        for sent in para:
+            sents = sents + sent[0]
+        sents = sents + "\n"
+        txt.append(sents)
+    filename =  "revised_"+config.get("TEXT","input_file")
+    f = open(filename,'w')
+    f.writelines(txt)
+    f.flush()
+    f.close()
 
-def rewriter(txt,σ=0.975,k=0.1,batch=1):
-    global progress_counter
-    # set minibatch size of this task, determine how many sentences will be created in one call.
-    progress_bar.clear()
-    progress_bar.box()
-    text = [txt] * batch
-    org_tokens = tokenizer(text, return_tensors="pt")
-    special_tokens = {"[CLS]": 0, "[UNK]": 0, "[MASK]": 0, "[SEP]": 0, "[PAD]": 0, "'": 0, '"': 0, ";": 0, ":": 0, ",": 0,
-                    ".": 0, "?": 0, "/": 0, ">": 0, "<": 0, "{": 0, "}": 0}
-    special_tokens = {k:tokenizer.convert_tokens_to_ids(k) for k,v in special_tokens.items()}
-    # determine all replaceable positions in the sentence.
-    mask_pos = []
-    for i in range(org_tokens["input_ids"][0].size(0)):
-        if org_tokens["input_ids"][0][i] not in special_tokens.values():
-            mask_pos.append(i)
-    # iteratively replace the mask words
-    i = 1
-    tokens = copy.deepcopy(org_tokens)
-    progress_bar.erase()
-    while len(mask_pos) > 0 and i <= len(tokens["input_ids"][0]):
-        i = i+1
-        progress_counter = progress_counter + 1
-        # pick a random word in k position and mask it
-        pos = random.sample(mask_pos,1)
-        mask_pos.remove(pos[0])
-        # calculate the prob of enforcement
-        Penforce = penforce(batch,pos,org_tokens,tokens,k,σ)
+def undoChange():
+    for sent in replaced_sents:
+        if sent[6] == txt_box.select_idx:
+            txt_box.getParagragh()[txt_box.current_para_idx][txt_box.current_sent_idx][0] = str(sent[0])
+            txt_box.getParagragh()[txt_box.current_para_idx][txt_box.current_sent_idx][7] = 0
+            replaced_sents.remove(sent)
+            break
 
-        # calculate the P(lm), it is a token_size * vocal_size matrix
-        replaced_word_idx = copy.deepcopy(tokens["input_ids"][:,pos[0]])
-        Plm = plm(pos,tokens)
+def main(stdscr):
+    global config,screen,txt_box,revised_box,σ,k,batch
+    # read config file
+    file_path = os.path.join(os.path.abspath("."), "rewriter.ini")
+    config.read(file_path)
+    σ = config.get("MODEL_PARAM",σ)
+    k = config.get("MODEL_PARAM",k)
+    batch = config.get("MODEL_PARAM",batch)
+    # set tbe screen
+    screen = stdscr
+    # curses initialization
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_RED)
+    curses.init_pair(2, curses.COLOR_CYAN, -1)
+    curses.init_pair(3, curses.COLOR_RED, -1)
+    curses.init_pair(4, curses.COLOR_BLUE, -1)
+    curses.curs_set(0)
+    # create text frame to display original sentences
+    txt_box = Textframe(stdscr,int(config.get('GUI','txtbox_line'))
+                        ,int(config.get('GUI','txtbox_col'))
+                        ,int(config.get('GUI','txtbox_y'))
+                        ,int(config.get('GUI','txtbox_x'))
+                        ,int(config.get('GUI','txtbox_id')))
+    txt_box.enterKey = refineSentence
+    txt_box.alt_s    = saveChange
+    txt_box.u        = undoChange
+    # create text frame to display revised sentences width=200,height=10,y=26,x=5
+    revised_box = Textframe(stdscr,int(config.get('GUI','revisedbox_line'))
+                        ,int(config.get('GUI','revisedbox_col'))
+                        ,int(config.get('GUI','revisedbox_y'))
+                        ,int(config.get('GUI','revisedbox_x'))
+                        ,int(config.get('GUI','revisedbox_id')))
+    revised_box.indent = 0
+    revised_box.enterKey = refineSentence
+    revised_box.alt_s = conformChange
+    txt_box.setText(readTXT(config.get('TEXT','input_file')))
+    txt_box.calcPosition(0)
+    txt_box.printText()
+    # revised_box.register(fun2)
+    txt_box.selectSentence()
 
-        # calculate the proposal prob
-        Pproposal = Plm + Penforce
-        ppl_word_idx = torch.topk(Pproposal,k=batch,dim=-1)
-        y = torch.diag_embed(torch.ones(batch,dtype=torch.long))
-        sample_idx = torch.sum(ppl_word_idx.indices * y,dim=-1)
-        index = [i for i in range(len(sample_idx))]
-        random.shuffle(index)
-
-        # print ("origin:{},ppl:{}".format(tokenizer.decode(replaced_word_idx)
-        #                                         ,tokenizer.decode(sample_idx)))
-        # replace the MASK with proposed words
-        tokens["input_ids"][:,pos[0]] = sample_idx
-        update_progress(progress_counter)
-    progress_counter = 0
-    return tokens
-
-if __name__ == "__main__":
-    main()
+if __name__=='__main__':
+    wrapper(main)
 
